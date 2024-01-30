e5281d077fc3804514c393582c1b0e91e5580fca
SergeyVolkoff <vol7745@yandex.ru>
2024-01-30 06:47:19 -0500

    sw

---
+++ start_gns_test/start_gns_test_GRE.py
@@ -0,0 +1,23 @@
+import time
+import sys
+import os
+
+sys.path.insert(1, os.path.join(sys.path[0],'..'))
+from constants_trident  import (
+    VALUE_CONS_CONNECT,
+    CONSOLE,
+    NAME_DEV,
+)
+
+from base_gns3 import Base_gns
+
+CONSOLE.print(
+    "Тест работает по ПМИ 'Проверка поддержки нескольких WAN-портов(mwan3)'.",
+    "\nРекомендуется ознакомиться с описанием теста.",
+    "\nВ ходе теста настройки устойства будут сброшены,",
+    "\nбудет запрошено название лабы gns3 и предложены варианты ответа",
+    style='info'
+              )
+time.sleep(5)
+current_lab = Base_gns() # test wait this lab - SSV_auto_BM10_MWAN
+print(current_lab.start_nodes_from_project())
