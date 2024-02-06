3c0de2564535a78d92041e0fcaa2cdaadce9c947
SergeyVolkoff <vol7745@yandex.ru>
2024-02-06 15:28:45 -0500

    sw

--- start_gns_test/start_gns_test_GRE.py
+++ start_gns_test/start_gns_test_GRE.py
@@ -1,11 +1,12 @@
 import time
 import sys
 import os
+import pytest
 
 import yaml
 
 sys.path.insert(1, os.path.join(sys.path[0],'..'))
-print(sys.path)
+# print(sys.path)
 from constants_trident  import (
     VALUE_CONS_CONNECT,
     CONSOLE,
@@ -17,30 +18,35 @@ from base_gns3 import Base_gns
 from connect import Connect
 from cfg_switch import TridentCfg
 
-CONSOLE.print(
-    "Тест работает по ПМИ 'Проверка поддержки GRE'(Jira:TRIDENT2-1886).",
-    "\nРекомендуется ознакомиться с описанием теста.",
-    "\nВ ходе теста настройки устойства будут сброшены,",
-    "\nбудет запрошено название лабы gns3 и предложены варианты ответа",
-    style='info'
-              )
-time.sleep(5)
-current_lab = Base_gns() # test wait this lab - SSV_auto_BM10_MWAN
-print(current_lab.start_nodes_from_project())
+# CONSOLE.print(
+#     "\nТест работает по ПМИ 'Проверка поддержки GRE'.",
+#     "\nРекомендуется ознакомиться с описанием теста в ПМИ (Jira:TRIDENT2-1886).",
+#     "\nВ ходе теста настройки устойства будут сброшены,",
+#     "\nбудет запрошено название подключаемой схемы gns3 и предложены варианты ответа",
+#     style='info'
+#               )
+# time.sleep(7)
+# current_lab = Base_gns() # test wait this lab - SSV_auto_BM10_MWAN
+# print(current_lab.start_nodes_from_project())
 
-CONSOLE.print("Стартует сброс конфига DUT перед настройкой под тест\n",
-              style='info')
-time.sleep(5)
-# Создание об-а для подключения по консоли
+# CONSOLE.print("\nСтартует сброс настроек DUT на дефолтные\n",
+#               style='info')
+# time.sleep(5)
+# # Создание об-а для подключения по консоли
 tr1 = TridentCfg()
 # Сброс конфига
-print(tr1.extended_reset_cfg())
+# print(tr1.extended_reset_cfg())
 
 CONSOLE.print(
-       "Стартует настройка DUT под тест 'Проверка поддержки протокола GRE'\n",
+       "\nСтартует настройка DUT под тест 'Проверка поддержки протокола GRE'\n",
        style='info')
 time.sleep(5)
 # команды настройки конфига 
 with open("../templates_cfg/cfg_GRE.yaml") as commands:
         commands_template = yaml.safe_load(commands)
 print(tr1.cfg_base(commands_template))
+
+CONSOLE.print("\nСтартует настройка Pytests под тест 'Проверка поддержки протокола GRE'\n",
+              style='info')
+time.sleep(10)
+pytest.main(["-v","--html=BULAT_TEST_TRIDENT_GRE.html","../tests/test_check_gre.py"])
