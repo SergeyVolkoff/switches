0a3b553f1d6c0697df536a77f5769ff89bc66e8c
SergeyVolkoff <vol7745@yandex.ru>
2024-02-06 15:29:19 -0500

    sw

--- start_gns_test/start_gns_test_GRE.py
+++ start_gns_test/start_gns_test_GRE.py
@@ -18,24 +18,24 @@ from base_gns3 import Base_gns
 from connect import Connect
 from cfg_switch import TridentCfg
 
-# CONSOLE.print(
-#     "\nТест работает по ПМИ 'Проверка поддержки GRE'.",
-#     "\nРекомендуется ознакомиться с описанием теста в ПМИ (Jira:TRIDENT2-1886).",
-#     "\nВ ходе теста настройки устойства будут сброшены,",
-#     "\nбудет запрошено название подключаемой схемы gns3 и предложены варианты ответа",
-#     style='info'
-#               )
-# time.sleep(7)
-# current_lab = Base_gns() # test wait this lab - SSV_auto_BM10_MWAN
-# print(current_lab.start_nodes_from_project())
+CONSOLE.print(
+    "\nТест работает по ПМИ 'Проверка поддержки GRE'.",
+    "\nРекомендуется ознакомиться с описанием теста в ПМИ (Jira:TRIDENT2-1886).",
+    "\nВ ходе теста настройки устойства будут сброшены,",
+    "\nбудет запрошено название подключаемой схемы gns3 и предложены варианты ответа",
+    style='info'
+              )
+time.sleep(7)
+current_lab = Base_gns() # test wait this lab - SSV_auto_BM10_MWAN
+print(current_lab.start_nodes_from_project())
 
-# CONSOLE.print("\nСтартует сброс настроек DUT на дефолтные\n",
-#               style='info')
-# time.sleep(5)
-# # Создание об-а для подключения по консоли
+CONSOLE.print("\nСтартует сброс настроек DUT на дефолтные\n",
+              style='info')
+time.sleep(5)
+# Создание об-а для подключения по консоли
 tr1 = TridentCfg()
 # Сброс конфига
-# print(tr1.extended_reset_cfg())
+print(tr1.extended_reset_cfg())
 
 CONSOLE.print(
        "\nСтартует настройка DUT под тест 'Проверка поддержки протокола GRE'\n",
